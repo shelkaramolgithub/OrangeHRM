20c6de4a02c24217145381d69fb23702e6404e35
sam <somnathanytime@gmail.com>
2022-03-31 19:20:38 +0530

    First Commit: First Test case save

---
+++ OHRM/src/TestcasesAmol/TestcaseSample.java
@@ -0,0 +1,10 @@
+package TestcasesAmol;
+
+public class TestcaseSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sample test case for github");
+	}
+
+}

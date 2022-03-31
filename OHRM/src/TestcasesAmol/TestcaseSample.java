fefc0a69065cdd4a6d66ca47b9bbad0ebaf92080
dell <dell@MUL>
2022-03-31 21:43:27 +0530

    test updated

--- OHRM/src/TestcasesAmol/TestcaseSample.java
+++ OHRM/src/TestcasesAmol/TestcaseSample.java
@@ -5,6 +5,8 @@ public class TestcaseSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Sample test case 1 for github");
+
+System.out.println("Test case updated");
 	}
 
 }

405f16236ef1da24da5a32700facc2c7cca14416
ADMIN <ADMIN@DESKTOP-AVPSG7U>
2022-04-01 14:46:45 +0530

    test case 2 for github has been added

---
+++ OHRM/src/TestcasesAmol/TestcaseAruna.java
@@ -0,0 +1,14 @@
+package TestcasesAmol;
+
+public class TestcaseAruna {
+
+	public static void main(String[] args)
+	
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is test case 2 for Github");
+
+	}
+
+}
